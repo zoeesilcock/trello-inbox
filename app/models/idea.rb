7646e476b7c49087cf9cad4dd7d38872f15d0855
Zoee Silcock <mrzoee@gmail.com>
2015-11-02 15:53:39 +0100

    Create a card in Trellow when an Idea is created

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,15 @@ class Idea < ActiveRecord::Base
   belongs_to :inbox
   validates :title, presence: true
   validates :inbox, presence: true
+  after_create :create_in_trello
+
+  private
+
+  def create_in_trello
+    Trello::Card.create(
+      name: title,
+      description: description,
+      list_id: inbox.board.lists.first.id
+    )
+  end
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,13 +3,29 @@ require 'rails_helper'
 RSpec.describe Idea, type: :model do
   let (:idea) { build :idea }
 
-  it 'requires a title' do
-    idea.title = nil
-    expect(idea).not_to be_valid
+  describe 'validations' do
+    it 'requires a title' do
+      idea.title = nil
+      expect(idea).not_to be_valid
+    end
+
+    it 'requires an inbox' do
+      idea.inbox_id = nil
+      expect(idea).not_to be_valid
+    end
   end
 
-  it 'requires an inbox' do
-    idea.inbox_id = nil
-    expect(idea).not_to be_valid
+  describe 'callbacks' do
+    let (:board) { double(:board, lists: [double(:list, id: "1")]) }
+
+    it 'creates the card in trello' do
+      expect(Trello::Board).to receive(:find).and_return(board)
+      expect(Trello::Card).to receive(:create).with(hash_including(
+        name: idea.title,
+        description: idea.description
+      )).and_return(nil)
+
+      idea.save
+    end
   end
 end

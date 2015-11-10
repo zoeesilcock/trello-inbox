5d98b679da305131312a770331ba59b434854551
Zoee Silcock <mrzoee@gmail.com>
2015-11-10 23:00:52 +0100

    Fix some rubocop warnings due to react_on_rails

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -4,16 +4,20 @@ ReactOnRails.configure do |config|
 
   # Server rendering:
   # Server bundle is a single file for all server rendering of components.
-  # It is important to set this to "" if you are not doing server rendering to avoid an extraneous log warning
-  # that the default file of server-bundle.js does not exist.
-  config.server_bundle_js_file = "app/assets/javascripts/generated/server-bundle.js" # This is the default
+  # It is important to set this to "" if you are not doing server rendering to
+  # avoid an extraneous log warning that the default file of server-bundle.js
+  # does not exist.
+  config.server_bundle_js_file =
+    'app/assets/javascripts/generated/server-bundle.js' # This is the default
   # increase if you're on JRuby
   config.server_renderer_pool_size = 1
   # seconds
   config.server_renderer_timeout = 20
-  # If set to true, this forces Rails to reload the server bundle if it is modified
+  # If set to true, this forces Rails to reload the server bundle if it is
+  # modified
   config.development_mode = Rails.env.development?
-  # For server rendering. This can be set to false so that server side messages are discarded.
+  # For server rendering. This can be set to false so that server side
+  # messages are discarded.
   # Default is true. Be cautious about turning this off.
   config.replay_console = true
   # Default is true. Logs server rendering messags to Rails.logger.info

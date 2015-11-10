542daf6c0274bc1f2e2a053aed629ba57daf9475
Zoee Silcock <mrzoee@gmail.com>
2015-11-10 22:40:38 +0100

    Filter ideas based on ownership

--- client/app/bundles/Ideas/components/IdeasFilterComponent.jsx
+++ client/app/bundles/Ideas/components/IdeasFilterComponent.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import I18n from 'i18n-js';
 import { Input } from 'react-bootstrap';
 
 export default class IdeasFilterComponent extends React.Component {
+  static propTypes = {
+    filter: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  }
+
   render() {
     return (
       <div>
         <form className="form-inline pull-left filter-form">
-          <Input type="select" label={I18n.t('ideas.index.filter.label')} placeholder="select">
+          <Input type="select" label={I18n.t('ideas.index.filter.label')} placeholder="select" defaultValue={this.props.filter} onChange={this.props.onChange}>
             <option value="all">{I18n.t('ideas.index.filter.all')}</option>
             <option value="mine">{I18n.t('ideas.index.filter.mine')}</option>
           </Input>
---
+++ client/app/bundles/Ideas/stores/IdeasFilterStore.jsx
@@ -0,0 +1,18 @@
+import alt from '../alt';
+import IdeasFilterActions from '../actions/IdeasFilterActions';
+
+class IdeasFilterStore {
+  constructor() {
+    this.filter = 'all';
+
+    this.bindListeners({
+      handleUpdateFilter: IdeasFilterActions.UPDATE_FILTER
+    });
+  }
+
+  handleUpdateFilter(filter) {
+    this.filter = filter;
+  }
+}
+
+export default alt.createStore(IdeasFilterStore, 'IdeasFilterStore');

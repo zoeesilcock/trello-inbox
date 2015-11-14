6359b4b1eee9e6f1f514c547563217d7c09c18e4
Zoee Silcock <mrzoee@gmail.com>
2015-11-14 14:59:01 +0100

    Manually bind the store
    I'm doing this for now until I can figure out a cleaner way of
    injecting initial data into an Alt store. I bet there is a way but I
    don't have an internet connection right now.

--- client/app/bundles/Users/containers/UsersContainer.jsx
+++ client/app/bundles/Users/containers/UsersContainer.jsx
@@ -1,35 +1,36 @@
 import React, { PropTypes } from 'react';
-import connectToStores from 'alt/utils/connectToStores';
 import UsersListContainer from './UsersListContainer';
 import UsersActions from '../actions/UsersActions';
 import UsersStore from '../stores/UsersStore';
 
 class UsersContainer extends React.Component {
   static propTypes = {
-    users: PropTypes.array.isRequired
+    initial_users: PropTypes.array.isRequired
   }
 
-  static getStores() {
-    return [UsersStore];
-  }
+  state = UsersStore.getState()
 
-  static getPropsFromStores() {
-    return UsersStore.getState();
-  }
-
-  constructor(props, context, updater) {
-    super(props, context, updater);
+  componentDidMount() {
+    UsersStore.listen(this.onChange.bind(this));
     UsersActions.receiveData(this.props.initial_users);
+  }
+
+  componentWillUnmount() {
+    UsersStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
   }
 
   render() {
     return (
       <div>
         <h1>{I18n.t('users.index.users')}</h1>
-        <UsersListContainer users={this.props.users} />
+        <UsersListContainer users={this.state.users} />
       </div>
     );
   }
 }
 
-export default connectToStores(UsersContainer);
+export default UsersContainer;

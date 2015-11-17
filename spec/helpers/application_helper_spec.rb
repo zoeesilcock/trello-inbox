5d7817c70c1f2008809968ab10a0852ba4de5c0b
Zoee Silcock <mrzoee@gmail.com>
2015-11-16 19:35:03 -0600

    Add specs for the current_inbox helper method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,5 +2,23 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#current_inbox' do
+    context 'with an inbox_id' do
+      let!(:inbox) { create :inbox }
+      let(:params) { { inbox_id: inbox.id } }
+
+      before do
+        expect(helper).to receive(:params).twice.and_return(params)
+      end
+
+      it 'returns the inbox' do
+        expect(helper.current_inbox).to eq(inbox)
+      end
+    end
+
+    context 'without an inbox_id' do
+      it 'returns nil' do
+        expect(helper.current_inbox).to be_nil
+      end
+    end
   end
 end

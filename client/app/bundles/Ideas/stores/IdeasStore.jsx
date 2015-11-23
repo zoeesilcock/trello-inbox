87456fa747bbc68786927f6195cc5e1aadb31239
Zoee Silcock <mrzoee@gmail.com>
2015-11-23 20:25:56 +0100

    Index ideas using lunr.js for search

--- client/app/bundles/Ideas/stores/IdeasStore.jsx
+++ client/app/bundles/Ideas/stores/IdeasStore.jsx
@@ -1,8 +1,21 @@
 import alt from '../alt';
+var lunr = require('lunr');
 
 class IdeasStore {
   constructor() {
     this.ideas = [];
+
+    this.on('bootstrap', () => {
+      this.index = lunr(function() {
+        this.ref('id');
+        this.field('title', { boost: 10 });
+        this.field('description');
+      });
+
+      for (var i = 0; i < this.ideas.length; i++) {
+        this.index.add(this.ideas[i]);
+      }
+    });
   }
 }
 

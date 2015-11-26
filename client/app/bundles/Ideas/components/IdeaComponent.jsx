da7e94043c87a1d6d8bd46049a9d09626f4d4d32
Zoee Silcock <mrzoee@gmail.com>
2015-11-26 19:39:47 +0100

    Stretch the flexboxes vertically for consistency

--- client/app/bundles/Ideas/components/IdeaComponent.jsx
+++ client/app/bundles/Ideas/components/IdeaComponent.jsx
@@ -23,7 +23,7 @@ export default class IdeaComponent extends React.Component {
 
   render() {
     return (
-      <li className="flex-child" key={this.props.index}>
+      <li className="flex-child fill-height" key={this.props.index}>
         <div className="idea-container" onClick={this.props.onShow}>
           <div className="idea-header">
             <img src={this.props.idea.user_avatar} alt={this.props.idea.user_name} className="avatar" />

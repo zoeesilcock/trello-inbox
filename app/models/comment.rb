c23007e643e37bb148ac6aedfde242d3f07849f5
Zoee Silcock <mrzoee@gmail.com>
2015-12-25 19:57:22 +0100

    Present comments created by us properly

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,26 @@ class Comment < ActiveRecord::Base
   belongs_to :idea
   validates :text, presence: true
 
+  def header
+    I18n.t('comments.used_trello_inbox_to_say')
+  end
+
   def create_in_trello(user)
     card = Trello::Card.find idea.card_id
-    header = I18n.t('comments.used_trello_inbox_to_say')
     comment = card.add_comment "#{user.name} #{header}:\n\n#{text}"
     self.comment_id = JSON.parse(comment)['id']
     save
+  end
+
+  def text
+    self[:text].gsub(/.* #{header}\:\n\n/, '') if self[:text]
+  end
+
+  def avatar
+    if user_avatar =~ %r{https?\:\/\/}
+      user_avatar
+    else
+      "https://trello-avatars.s3.amazonaws.com/#{user_avatar}/170.png"
+    end
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -36,4 +36,29 @@ RSpec.describe Comment, type: :model do
       comment.create_in_trello(user)
     end
   end
+
+  describe '#text' do
+    it 'removes the header' do
+      comment.text = "Some weirdo used Trello Inbox to say:\n\nHello :)"
+      expect(comment.text).to eq('Hello :)')
+    end
+  end
+
+  describe '#avatar' do
+    context 'with a trello hash avatar' do
+      it 'adds the trello avatar url' do
+        comment.user_avatar = '12a4f0297fe2ecb7275048e89c47ec8f'
+        expect(comment.avatar).to include('trello-avatars.s3.amazonaws.com')
+      end
+    end
+
+    context 'with a URL avatar' do
+      let(:url) { 'https://some-service.com/my_avatar.jpg' }
+
+      it 'returns the url as it appears on the model' do
+        comment.user_avatar = url
+        expect(comment.avatar).to eq(url)
+      end
+    end
+  end
 end
